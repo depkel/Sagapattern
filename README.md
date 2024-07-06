aeaaf7fe0eebd76ca956df83d8d05ca8071c6623
depkel <depkel@hotmail.com>
2024-07-06 12:40:35 +0400

    test

---
+++ README.md
@@ -0,0 +1,14 @@
+# SimpleSaga
+
+A Typescript project that demonstrates how to implement the Saga pattern. Two approaches are used: Orchestration and Choreography.
+
+The Orchestration approach is implemented in the [`orchestration`](./orchestration) folder. 
+
+The Choreography approach is implemented in the [`choreography`](./choreography) folder.
+
+Read the contents of each folder to learn the details about installing and running the code for the respetive project.
+
+|IMPORTANT|
+|---------|
+|Make sure you've installed all the dependencies and libraries required to run a Typescript project. Run the following command to install Typescript:<br><br>`npm install --save-dev typescript`|
+
---
+++ orchestration/src/reservation/AbstractReservation.ts
@@ -0,0 +1,30 @@
+import {ICustomer} from './ICustomer';
+import {IConfirmation} from './IConfirmation';
+import {v4 as uuidv4} from 'uuid';
+import * as emoji from 'node-emoji';
+
+export abstract class AbstractReservation {
+  public async reserve(customer: ICustomer): Promise<IConfirmation> {
+    // create a random number either 0, 1 or 2
+    
+    const random = Math.floor(Math.random() * 3);
+    if (random === 0)
+      throw new Error(
+        `Oh no! Something went wrong in ${this.constructor.name}!\n`
+      );
+
+    const str = `Making ${this.constructor.name} reservation for customer ${customer.firstName} ${customer.lastName}`;
+    console.log(str);
+    return {
+      uuid: uuidv4(),
+      confirmationDate: new Date(),
+      reservation: this,
+    };
+  }
+  public async compensate(): Promise<void> {
+    // Console.log the class name
+    console.log(
+      `${emoji.get('skull')} Compensating ${this.constructor.name} reservation`
+    );
+  }
+}
